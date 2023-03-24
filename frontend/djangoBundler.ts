01183bd28484103a2281655cc5733e983b84855b
faucomte97 <f.aucomte@hotmail.co.uk>
2023-03-24 22:51:57 +0000

    Linting improvements

--- frontend/djangoBundler.ts
+++ frontend/djangoBundler.ts
@@ -1,10 +1,10 @@
-const Bundler = require('parcel-bundler')
-const Path = require('path')
-const fs = require('fs')
-const Handlebars = require('handlebars')
+import Bundler from 'parcel-bundler';
+import Handlebars from 'handlebars';
+import Path from 'path';
+import fs from 'fs';
 
-const file = Path.join(__dirname, './public/index.html')
-const outDir = Path.join(__dirname, '../backend/myapp/static/react')
+const file = Path.join(__dirname, './public/index.html');
+const outDir = Path.join(__dirname, '../backend/myapp/static/react');
 
 const options = {
   outDir,
@@ -13,50 +13,50 @@ const options = {
   watch: process.env.NODE_ENV !== 'production',
   minify: process.env.NODE_ENV === 'production',
   target: 'browser',
-  cache: process.env.NODE_ENV === 'production',
-}
+  cache: process.env.NODE_ENV === 'production'
+};
 
-const templateFolder = Path.resolve(Path.join(__dirname, '../backend/myapp/templates'))
+const templateFolder = Path.resolve(Path.join(__dirname, '../backend/myapp/templates'));
 const handlebarsTemplatePath = Path.resolve(
   Path.join(__dirname, './public/handlebars_template.html')
-)
+);
 
-const bundler = new Bundler(file, options)
+const bundler = new Bundler(file, options);
 
-function getReactURL(entryPointHTML) {
+function getReactURL(entryPointHTML: string) {
   const regex =
-    /(<script src="\/static\/react\/)(.*\.js)("><\/script>\n?)/g
-  return regex.exec(entryPointHTML)[2]
+    /(<script src="\/static\/react\/)(.*\.js)("><\/script>\n?)/g;
+  return regex.exec(entryPointHTML)[2];
 }
 
-Handlebars.registerHelper('if_eq', function (a, b, opts) {
+Handlebars.registerHelper('if_eq', function (a: any, b: any, opts: any) {
   if (a === b) {
-    return opts.fn(this)
+    return opts.fn(this);
   } else {
-    return opts.inverse(this)
+    return opts.inverse(this);
   }
-})
+});
 
-function generateHTML(reactFileName) {
-  const templateString = fs.readFileSync(handlebarsTemplatePath, 'utf-8')
-  const template = Handlebars.compile(templateString)
+function generateHTML(reactFileName: string): string {
+  const templateString = fs.readFileSync(handlebarsTemplatePath, 'utf-8');
+  const template = Handlebars.compile(templateString);
   return template({
     reactUrl: `react/${reactFileName}`,
-    environment: process.env.SERVER_ENV,
-  })
+    environment: process.env.SERVER_ENV
+  });
 }
 
-bundler.on('bundled', (bundle) => {
-  const entryPointHTML = fs.readFileSync(bundle.name, 'utf-8')
-  const reactUrl = getReactURL(entryPointHTML)
-  const HTML = generateHTML(reactUrl)
+bundler.on('bundled', (bundle: any) => {
+  const entryPointHTML = fs.readFileSync(bundle.name, 'utf-8');
+  const reactUrl = getReactURL(entryPointHTML);
+  const HTML = generateHTML(reactUrl);
 
-  fs.writeFile(`${templateFolder}/my_app_react.html`, HTML, (error) => {
+  fs.writeFile(`${templateFolder}/my_app_react.html`, HTML, (error: any) => {
     if (error) {
-      return console.log(error)
+      console.log(error);
     }
-    console.log('my_app_react.html django template generated sucessfully')
-  })
-})
+    console.log('my_app_react.html django template generated successfully');
+  });
+});
 
-bundler.bundle()
+bundler.bundle();
