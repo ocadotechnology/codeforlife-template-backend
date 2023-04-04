7456f4d239f428247f9e915092db977f83253e69
SKairinos <stefan.kairinos@ocado.com>
2023-04-04 13:49:56 +0100

    map to all urls

--- backend/myapp/urls.py
+++ backend/myapp/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r"^$", views.render_react, name="react_app"),
+    url(r".*", views.render_react, name="react_app"),
 ]
