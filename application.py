01c0129c5408c450e2ae777760bdef4db946694f
SKairinos <skairinos@gmail.com>
2025-06-25 13:24:04 +0000

    fix: server

--- application.py
+++ application.py
@@ -5,8 +5,6 @@ Created on 11/04/2024 at 16:51:45(+01:00).
 The entrypoint to our app.
 """
 
-from codeforlife.servers import CeleryServer, DjangoServer
+from codeforlife.server import Server
 
-celery_app = CeleryServer().app
-
-django_app = DjangoServer().wsgi_app
+Server().run()
